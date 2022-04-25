0332cf778ad9c32c09a3cdd21cffe8fbef54a1f2
MaRaMinden <matt.ramin@gmail.com>
2022-04-25 17:38:00 +0200

    Added empty check

--- mensa-bot.py
+++ mensa-bot.py
@@ -1,13 +1,6 @@
-# Token: 5390138179:AAEEFYigTsS2djDkOKRLfYbIzW6XNSOnNyk
-
-'''
-
-
-'''
 import random
 import requests
 from datetime import datetime
-from datetime import timedelta, date
 
 greeting_text_snippets = [
 	"Ahoi ihr Landratten! Heute gibt's mal wieder leckerschmecker Essen für euch, und zwar Folgendes:",
@@ -50,13 +43,13 @@ def roll_emoji(emoji_list):
 	return emojicode
 
 def telegram_bot_sendtext(bot_message):
-	bot_token = '5390138179:AAEEFYigTsS2djDkOKRLfYbIzW6XNSOnNyk'
+	bot_token = ''
 	bot_chatID = '@lg_bester_mensabot'
 	send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
 	response = requests.get(send_text)
 
-	send_animation = 'https://api.telegram.org/bot' + bot_token + '/sendAnimation?chat_id=' + bot_chatID + '&animation=https://media.giphy.com/media/l4KibK3JwaVo0CjDO/giphy.gif'
-	#response2 = requests.get(send_animation)
+	# send_animation = 'https://api.telegram.org/bot' + bot_token + '/sendAnimation?chat_id=' + bot_chatID + '&animation=https://media.giphy.com/media/l4KibK3JwaVo0CjDO/giphy.gif'
+	# response2 = requests.get(send_animation)
 
 	return response.json()
 
@@ -79,17 +72,20 @@ meal_message += random_greeting_text + "\n"
 
 def add_meal_strings(meal_list):
 	string = ""
-	for current_meal in meal_list:
-		current_meal_name = current_meal['name']
-		current_prices = current_meal['price']
-		current_student_price = (current_prices['student'] + "€").replace(".", ",")
-		"""
-		current_employee_price = (current_prices['employee'] + "€").replace(".", ",")
-		current_guest_price = (current_prices['guest'] + "€").replace(".", ",")
-		"""
-		current_meal_prices_string = "(" + current_student_price + ")"
+	if len(meal_list) == 0:
+		string += "\nHm, hier gibt's heute nüscht.."
+	else:
+		for current_meal in meal_list:
+			current_meal_name = current_meal['name']
+			current_prices = current_meal['price']
+			current_student_price = (current_prices['student'] + "€").replace(".", ",")
+			"""
+			current_employee_price = (current_prices['employee'] + "€").replace(".", ",")
+			current_guest_price = (current_prices['guest'] + "€").replace(".", ",")
+			"""
+			current_meal_prices_string = "(" + current_student_price + ")"
 
-		string = string + "\n" + current_meal_name + " " + current_meal_prices_string
+			string = string + "\n" + current_meal_name + " " + current_meal_prices_string
 	return string + "\n"
 
 meal_message += "\n" + roll_emoji(vegan_emojis) + " *VEGAN*:"
@@ -102,5 +98,5 @@ meal_message += "\n" + roll_emoji(meat_emojis) + " *FLEISCH*:"
 meal_message += add_meal_strings(asi_meals)
 
 meal_message += "\nLasst's euch schmecken! \U0001F49A\nEuer Leuphana Mensabot \U0001f916"
-# print(meal_message)
-telegram_bot_sendtext(meal_message)
+print(meal_message)
+# telegram_bot_sendtext(meal_message)

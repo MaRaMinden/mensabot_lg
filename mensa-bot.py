7010bff58f02f8fd15adcc397179af98a21463de
MaRaMinden <matt.ramin@gmail.com>
2023-02-16 18:50:00 +0100

    Ah ups hehe hab was vergessen

--- mensa-bot.py
+++ mensa-bot.py
@@ -7,20 +7,20 @@ from datetime import datetime, timedelta
 # ö: "+u"\u00F6"+"
 
 greeting_text_snippets = [
-	"Ahoi ihr Landratten! Heute gibt's mal wieder leckerschmecker Essen f"+u"\u00FC"+"r euch, und zwar:",
+	"Ahoi ihr Landratten! Heute gibt's mal wieder leckerschmecker Essen f" + u"\u00FC" + "r euch, und zwar:",
 	"Wetter is mal wieder nich so? Vorlesung auch langweilig? Immerhin - Essen gibt's:",
-	"Das heutige Mittagessen ist allen gewidmet, die Dank Klausurenphase erst nach 4 Kaffee oder Mate etwas spüren. Ihr macht das super, haltet durch! "+u"\U0001F49A",
-	"Das heutige Mittagessen ist allen gewidmet, die Dank Klausurenphase erst nach 4 Kaffee oder Mate etwas spüren. Ihr macht das super, haltet durch! "+u"\U0001F49A",
-	"Rumort's in der Magengegend aber dein Crush ist gar nicht in der N"+u"\u00E4"+"he? Dann hilft vielleicht eine dieser Leckereien:",
-	"B"+u"\u00E4"+"ndernde haben Hunger, bitte um Sachspende:",
+	"Das heutige Mittagessen ist allen gewidmet, die Dank Klausurenphase erst nach 4 Kaffee oder Mate etwas spüren. Ihr macht das super, haltet durch! " + u"\U0001F49A",
+	"Das heutige Mittagessen ist allen gewidmet, die Dank Klausurenphase erst nach 4 Kaffee oder Mate etwas spüren. Ihr macht das super, haltet durch! " + u"\U0001F49A",
+	"Rumort's in der Magengegend aber dein Crush ist gar nicht in der N" + u"\u00E4" + "he? Dann hilft vielleicht eine dieser Leckereien:",
+	"B" + u"\u00E4" + "ndernde haben Hunger, bitte um Sachspende:",
 	"Dieses leckere Essen kannst du heute wahlweise mit Gabel oder mit Spoun essen:",
 	"Na, schon hungrig? Bisschen dauert's leider noch, aber ab 11:45 gibt's diesen Schmackofatz:",
-	"Ihr findet die Witze hier bl"+u"\u00F6"+"d? Ist wohl Geschmackssache. So wie das hier:",
-	"Wer wegen der RPO-"+u"\u00C4"+"nderungen im Strahl kotzen musste, kann hiermit wieder aufstocken:",
-	"Wer wegen der RPO-"+u"\u00C4"+"nderungen im Strahl kotzen musste, kann hiermit wieder aufstocken:",
-	"Die Klausurenphase ist hart, aber euer Hunger ist h"+u"\u00E4"+"rter? Kein Problem:",
-	"Die Klausurenphase ist hart, aber euer Hunger ist h"+u"\u00E4"+"rter? Kein Problem:",
-	"G"+u"\u00F6"+"nnt euch die Mensung für heute:"
+	"Ihr findet die Witze hier bl" + u"\u00F6" + "d? Ist wohl Geschmackssache. So wie das hier:",
+	"Wer wegen der RPO-" + u"\u00C4" + "nderungen im Strahl kotzen musste, kann hiermit wieder aufstocken:",
+	"Wer wegen der RPO-" + u"\u00C4" + "nderungen im Strahl kotzen musste, kann hiermit wieder aufstocken:",
+	"Die Klausurenphase ist hart, aber euer Hunger ist h" + u"\u00E4" + "rter? Kein Problem:",
+	"Die Klausurenphase ist hart, aber euer Hunger ist h" + u"\u00E4" + "rter? Kein Problem:",
+	"G" + u"\u00F6" + "nnt euch die Mensung für heute:"
 ]
 
 vegan_emojis = [
@@ -38,14 +38,15 @@ vegetarian_emojis = [
 ]
 meat_emojis = [
 	u"\U0001F644",
-	u"\U0001F9A7", # makaber
+	u"\U0001F9A7",  # makaber
 	u"\U0001F434",
 	u"\U0001F437",
 	u"\U0001F987",
 	u"\U0001F998",
-	u"\U0001F321", # Thermometer
-	u"\U0001F9F8" # Teddy
+	u"\U0001F321",  # Thermometer
+	u"\U0001F9F8"  # Teddy
 ]
+
 
 def roll_emoji(emoji_list):
 	emojicode = ""
@@ -54,8 +55,9 @@ def roll_emoji(emoji_list):
 
 	return emojicode
 
+
 def telegram_bot_sendtext(bot_message):
-	bot_token = ''
+	bot_token = '5390138179:AAEEFYigTsS2djDkOKRLfYbIzW6XNSOnNyk'
 	bot_chatID = '@lg_bester_mensabot'
 	send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
 	response = requests.get(send_text)
@@ -65,42 +67,33 @@ def telegram_bot_sendtext(bot_message):
 	print(response.json())
 	return response.json()
 
+
 # Get that sweet meal data
-mensa_id = "140" # 101 = Braunschweig; 140 = Lueneburg
-tomorrow_as_iso_string = (datetime.today()).strftime('%Y-%m-%d') # + timedelta(days=-1)
+mensa_id = "140"  # 101 = Braunschweig; 140 = Lueneburg
+tomorrow_as_iso_string = (datetime.today()).strftime('%Y-%m-%d')  # + timedelta(days=-1)
 meals = requests.get(
 	"https://sls.api.stw-on.de/v1/locations/" + mensa_id + "/menu/" + tomorrow_as_iso_string).json()['meals']
 
 # Split by meal type
-# vegan_meals = []
-# for meal in meals:
-# 	print(meal['tags'])
-# 	category_length = len(meal['tags']['categories'])
-# 	print(category_length)
-# 	# print(meal['tags']['categories'][0])
-# 	# print(meal['tags']['categories'][0]['name'])
-# 	if(category_length > 0 and meal['tags']['categories'][0]['name'] == 'Vegan'):
-# 		vegan_meals.append(meal)
-
-
 vegan_meals = list(filter(lambda current_meal: len(current_meal['tags']['categories']) > 0 and
-                                                current_meal['tags']['categories'][0]['name'] == 'Vegan', meals))
-
+                                               current_meal['tags']['categories'][0]['name'] == 'Vegan', meals))
 vegetarian_meals = list(filter(lambda current_meal: len(current_meal['tags']['categories']) > 0 and
-                                                    current_meal['tags']['categories'][0]['name'] == "Vegetarisch", meals))
+                                                    current_meal['tags']['categories'][0]['name'] == "Vegetarisch",
+                               meals))
 asi_meals = list(filter(lambda current_meal: len(current_meal['tags']['categories']) > 0 and
                                              current_meal['tags']['categories'][0]['name'] != "Vegetarisch" and
-											 current_meal['tags']['categories'][0]['name'] != "Vegan", meals))
+                                             current_meal['tags']['categories'][0]['name'] != "Vegan", meals))
 meal_message = ""
 
 # Add a random greeting
-random_greeting_text = greeting_text_snippets[random.randint(0, len(greeting_text_snippets)-1)]
+random_greeting_text = greeting_text_snippets[random.randint(0, len(greeting_text_snippets) - 1)]
 meal_message += random_greeting_text + "\n"
+
 
 def add_meal_strings(meal_list):
 	string = ""
 	if len(meal_list) == 0:
-		string += "\nHm, hier gibt's heute n"+u"\u00FC"+"scht.."
+		string += "\nHm, hier gibt's heute n" + u"\u00FC" + "scht.."
 	else:
 		previousPrice = 0.0
 		for current_meal in meal_list:
@@ -108,15 +101,16 @@ def add_meal_strings(meal_list):
 			current_student_price = (current_prices['student'] + u"\u20AC").replace(".", ",")
 			current_meal_name = current_meal['name']
 			current_price_as_float = float(current_prices['student'])
-			if(current_price_as_float < 2 ): # Check if it's just a sidedish
-				current_meal_name = "_" + current_meal_name + "_" # Make italic
-				if previousPrice > 2: # Add divider if category is not only side-dishes
+			if (current_price_as_float < 2):  # Check if it's just a sidedish
+				current_meal_name = "_" + current_meal_name + "_"  # Make italic
+				if previousPrice > 2:  # Add divider if category is not only side-dishes
 					string += "\n--------"
 			previousPrice = current_price_as_float
 			current_meal_prices_string = "(" + current_student_price + ")"
 
 			string = string + "\n" + current_meal_name + " " + current_meal_prices_string
 	return string + "\n"
+
 
 meal_message += "\n" + roll_emoji(vegan_emojis) + " *VEGAN*:"
 meal_message += add_meal_strings(vegan_meals)
